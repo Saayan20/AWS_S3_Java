beb1821bac57ce86e5940fe6c8bf9ea906f9ac38
abc <abc@example.com>
2020-02-13 02:17:19 +0530

    First Commit

---
+++ src/main/java/com/java/aws/constants/CommonConstants.java
@@ -0,0 +1,24 @@
+package com.java.aws.constants;
+
+public class CommonConstants {
+	
+	public static String ACCESS_KEY_ID = "AKIAZ4SP4CJRMXOONFVA";
+	
+	public static String ACCESS_SEC_KEY = "SvWbgWyeJ1kXBNd9B1qL8xRKCoGD7RH/WkuJbhNT";
+	
+	public static String BUCKET_NAME = "amazoncodedeploybucket";
+	
+	public static String FOLDER_NAME = "storeFiles";
+	
+	public static String FILE_NAME = "HelloWorld.txt";
+	
+	public static String FILE_PATH = "C:\\\\Users\\\\Sayan\\\\Desktop\\\\HelloWorld.txt";
+	
+	public static String BUCKET_FILE_PATH = "storeFiles/Invoice_May.pdf";
+	
+	public static String LOCAL_DOWNLOAD_PATH = "C:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\HelloWorld.txt_new.pdf";
+	
+	public static final String SUFFIX = "/";
+	
+
+}
